65b68026c67dfbd01eb175979059838c6cfdb139
chitra jaisimha <surajjayasimha@gmail.com>
2021-02-05 22:51:12 +0530

    Search Bar 2

--- lib/testapp.dart
+++ lib/testapp.dart
@@ -5,202 +5,23 @@ import 'package:flutter_search_bar/flutter_search_bar.dart';
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new MyHomePage(),
+    return Scaffold(
+      body: MyPage(),
+    );
       //   appBar: MyHomePage(),
       // floatingActionButton: FloatingActionButton(onPressed: (){ Navigator.pop(context);}),
-      // body: new ListView(
-      //   padding: const EdgeInsets.all(8),
-      //   children: <Widget>[
-      //     Container(
-      //       height: 600,
-      //       width: double.infinity,
-      //       color: Colors.black12,
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: <Widget>[
-      //           Container(
-      //             height: 80,
-      //             child: Text("People",
-      //                 style: TextStyle(
-      //                     fontSize: 40.0,
-      //                     fontWeight: FontWeight.bold,
-      //                     color: Colors.blue)),
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Vegetables", 1, Colors.blueGrey[200], "assets/images/Vegetables.png"),
-      //               SizedBox(
-      //                 height: 70,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Dhobi", 1, Colors.blueGrey[200], "assets/images/Dhibi.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Presswala", 1, Colors.blueGrey[200], "assets/images/Press.png"),
-      //               SizedBox(
-      //                 height: 70,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Maids", 1, Colors.blueGrey[200], "assets/images/Maid.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Drivers", 1, Colors.blueGrey[200], "assets/images/Driver.png"),
-      //               SizedBox(
-      //                 height: 70,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Security", 1, Colors.blueGrey[200], "assets/images/Security.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Plumbers", 1, Colors.blueGrey[200], "assets/images/Plumber.png"),
-      //               SizedBox(
-      //                 height: 70,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Electrician", 1, Colors.blueGrey[200], "assets/images/Electrician.png")
-      //                   ],
-      //           ),
-      //         ],
-      //       ),
-      //       ),
-      //     SizedBox(height: 50),
-      //     Container(
-      //       height: 600,
-      //       width: double.infinity,
-      //       color: Colors.black12,
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: <Widget>[
-      //           Container(
-      //             height: 80,
-      //             child: Text("Brick and Motar",
-      //                 style: TextStyle(
-      //                     fontSize: 40.0,
-      //                     fontWeight: FontWeight.bold,
-      //                     color: Colors.blue)),
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Hospitals", 1, Colors.blueGrey[200], "assets/images/Hospital.png"),
-      //               SizedBox(
-      //                 height: 100,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Pharmacy", 1, Colors.blueGrey[200], "assets/images/Pharmacy.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Hardware", 1, Colors.blueGrey[200], "assets/images/Hardware.png"),
-      //               SizedBox(
-      //                 height: 100,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Groceries", 1, Colors.blueGrey[200], "assets/images/Grocery.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Stationary", 1, Colors.blueGrey[200], "assets/images/Stationary.png"),
-      //             ],
-      //           ),
-      //
-      //         ],
-      //       ),
-      //     ),
-      //     SizedBox(height: 50),
-      //     Container(
-      //       height: 600,
-      //       width: double.infinity,
-      //       color: Colors.black12,
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: <Widget>[
-      //           Container(
-      //             height: 100,
-      //             child: Text("Events and Entertainment",
-      //                 textAlign: TextAlign.center,
-      //                 style: TextStyle(
-      //                     fontSize: 40.0,
-      //                     fontWeight: FontWeight.bold,
-      //                     color: Colors.blue)),
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Dancers", 1, Colors.blueGrey[200], "assets/images/Dance.png"),
-      //               SizedBox(
-      //                 height: 100,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Singers", 1, Colors.blueGrey[200], "assets/images/Singer.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Stand up", 1, Colors.blueGrey[200], "assets/images/Comedy.png"),
-      //               SizedBox(
-      //                 height: 100,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Organizers", 1, Colors.blueGrey[200], "assets/images/manager.png"),
-      //             ],
-      //           ),
-      //           SizedBox(height: 20),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               buildButton("Magicians", 1, Colors.blueGrey[200], "assets/images/Magic.png"),
-      //               SizedBox(
-      //                 height: 100,
-      //                 width: 20,
-      //               ),
-      //               buildButton("Anchors", 1, Colors.blueGrey[200], "assets/images/manager.png"),
-      //             ],
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //   ],
-      // ),
-    );
   }
 }
 
-class _MyHomePage extends StatefulWidget {
+class MyPage extends StatefulWidget {
   @override
-  __MyHomePageState createState() => __MyHomePageState();
+  _MyPageState createState() => _MyPageState();
 }
 
-class __MyHomePageState extends State<_MyHomePage> {
+class _MyPageState extends State<MyPage> {
   SearchBar searchBar;
 
-  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor,
-      String imageAsset) {
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor, String imageAsset) {
     return Container(
       padding: const EdgeInsets.all(0.0),
       height: 70 * buttonHeight,
@@ -234,11 +55,11 @@ class __MyHomePageState extends State<_MyHomePage> {
 
   AppBar buildAppBar(BuildContext context) {
     return new AppBar(
-        title: new Text('My Home Page'),
+        title: new Text('My Page'),
         actions: [searchBar.getSearchAction(context)]);
   }
 
-  _MyHomePageState() {
+  MyPageState() {
     searchBar = new SearchBar(
         inBar: false,
         setState: setState,
@@ -250,8 +71,7 @@ class __MyHomePageState extends State<_MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: searchBar.build(context),
-      body: new Center(
-        child: new ListView(
+      body:new ListView(
           padding: const EdgeInsets.all(8),
           children: <Widget>[
             Container(
@@ -447,7 +267,6 @@ class __MyHomePageState extends State<_MyHomePage> {
             ),
           ],
         ),
-      ),
-    );
+      );
   }
 }
